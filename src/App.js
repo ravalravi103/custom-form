ae7bd2bd248be5aeeebf57c15beb5ae89070eaf7
ravalravi103 <ravalravi103@gmail.com>
2020-11-27 14:43:29 +0530

    basic input type component done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 
+import React,{useState} from 'react'
+
 import fieldData from './data/data.json';
 
 function App() {
+  const [value,setValue] = useState('')
   return (
     <div className="App" >
-        {console.log(fieldData)}
+          {fieldData.map((field,i) => {
+            return (
+              <div key={i} className="input-card">
+                   <label>{field.UILabel}</label>
+                   <input 
+                    type={field.type}
+                    // placeholder={field.attributes.placeholder}
+                    // value={field.attributes.value}
+                    />
+              </div>
+            )
+          })}
     </div>
   );
 }
