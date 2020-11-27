0c33573c99ab40187749ea99b6e982ed4a47d082
ravalravi103 <ravalravi103@gmail.com>
2020-11-27 13:40:44 +0530

    data.json (data with diff input type)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
+import fieldData from './data/data.json';
+
 function App() {
   return (
-    <div className="App">
-       hello
+    <div className="App" >
+        {console.log(fieldData)}
     </div>
   );
 }
---
+++ src/data/test.js
@@ -0,0 +1,38 @@
+
+//   type : text
+//   type : number 
+//   type : email 
+//   type : file
+//   type : hidden 
+//   type : password 
+//   type : submit
+//   type : url
+//   type : time 
+//   type : week 
+//   type : date 
+//   type : Radio 
+//   type : image
+//   type : range 
+//   type : button
+//   type : reset
+//   type : tel
+
+
+//   [{
+//     "uiName": "Field 1",
+//     "fieldName": "field1",
+//     "type": "text",
+//     "attributes": {
+//       "placeholder": "Enter Field 1"
+//     }
+//   },
+//   {
+//     "uiName": "Field 2",
+//     "fieldName": "field2",
+//     "type": "number",
+//     "attributes": {
+//       "placeholder": "Enter Field 2",
+//       "min": 5,
+//       "max": 10
+//     }
+//   }]
