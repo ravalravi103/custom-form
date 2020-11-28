ca3da95b75a4dce4870407692d9a4ffd5531c6b2
ravalravi103 <ravalravi103@gmail.com>
2020-11-28 13:23:21 +0530

    all The input attributes done

--- src/App.js
+++ src/App.js
@@ -5,21 +5,53 @@ import React,{useState} from 'react'
 import fieldData from './data/data.json';
 
 function App() {
-  const [value,setValue] = useState('')
+  const [value,setValue] = useState('');
+
+  const onInputChange = (e) => {
+      setValue(e.target.value)
+  }
+  
   return (
     <div className="App" >
-          {fieldData.map((field,i) => {
+             <div>
+             <h1>Contact Form</h1>
+             {fieldData.map((field,i) => {
             return (
               <div key={i} className="input-card">
                    <label>{field.UILabel}</label>
                    <input 
                     type={field.type}
-                    // placeholder={field.attributes.placeholder}
-                    // value={field.attributes.value}
+                    onChange={(e) => onInputChange(e)}
+                    placeholder={field.attributes.placeholder}
+                    min={field.attributes.min}
+                    max={field.attributes.max}
+                    maxLength={field.attributes.maxLength}
+                    minLength={field.attributes.minLength}
+                    multiple={field.attributes.multiple}
+                    step={field.attributes.step}
+                    formAction={field.attributes.formaction}
+                    formEncType={field.attributes.formenctype}
+                    formNoValidate={field.attributes.fromonvalidate}
+                    formTarget={field.attributes.formTarget}
+                    fromonvalidate={field.attributes.fromonvalidate}
+                    src={field.attributes.src}
+                    width={field.attributes.width}
+                    height={field.attributes.height}
+                    alt={field.attributes.alt}
+                    required={field.attributes.required}
+                    checked={field.attributes.checked}
+                    pattern={field.attributes.pattern}
+                    list={field.attributes.list}
+                    value={field.attributes.value}
+                    name={field.attributes.name}
                     />
               </div>
             )
           })}
+             </div>
+
+
+          <div>{value}</div>
     </div>
   );
 }
